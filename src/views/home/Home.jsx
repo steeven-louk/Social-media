90a410c8b492c4072e727c5d6ef6f63750126eba
steeven-louk <steevenlouk.sl@gmail.com>
2022-10-25 18:21:15 +0200

    ajout de la section story

--- src/views/home/Home.jsx
+++ src/views/home/Home.jsx
@@ -10,7 +10,36 @@ const Home = () => {
         <div className="home">
             <SidebarLeft/>
             <div className="container">
-                homr
+                
+                <div className="story-gallery">
+                  <div className="story">
+                    <img src="./images/upload.png" alt="upload" className='upload' />
+                    <img src="./images/status-1.png" alt="" className='profil' />
+                    <p>Post Story</p>
+                  </div>
+
+                  <div className="story profil">
+                    <img src="./images/member-1.png" alt="member-1" className='upload' />
+                    <img src="./images/status-2.png" alt="" className='profil' />
+                    <p>Post Story</p>
+                  </div>
+                  <div className="story profil">
+                    <img src="./images/member-2.png" alt="member-1" className='upload' />
+                    <img src="./images/status-3.png" alt="" className='profil' />
+                    <p>Post Story</p>
+                  </div>
+                  <div className="story profil">
+                    <img src="./images/member-3.png" alt="member-1" className='upload' />
+                    <img src="./images/status-4.png" alt="" className='profil' />
+                    <p>Post Story</p>
+                  </div>
+                  <div className="story profil">
+                    <img src="./images/member-4.png" alt="member-1" className='upload' />
+                    <img src="./images/status-5.png" alt="" className='profil' />
+                    <p>Post Story</p>
+                  </div>
+                </div>
+
             </div>
             <SidebarRight />
         </div>
