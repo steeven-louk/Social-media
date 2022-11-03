da465adf80bc3d16b0bb6dd91b4966e3abc50b6b
steeven-louk <steevenlouk.sl@gmail.com>
2022-11-03 18:15:03 +0100

    mise en pause de la partie front-end du projet social-media

--- src/components/Post/PostCard.jsx
+++ src/components/Post/PostCard.jsx
@@ -1,180 +1,57 @@
-import React from 'react'
+import React from "react";
 
-import './style.scss';
+import "./style.scss";
 
-import {Link} from 'react-router-dom'
+import { Link } from "react-router-dom";
 
-const PostCard = () => {
+const PostCard = (props) => {
+
+  const {post} = props;
+
+
   return (
     <>
-        <div className="card">
-      <div className="card__user-profile">
-        <Link to="/profile">
+      <div className="card" key={post?.id}>
+        <div className="card__user-profile">
+          <Link to="/profile">
             <img src="./images/profile-pic.png" alt="" />
-        </Link>
-        <div className="card__user-name">
-          <p>John Nicholson</p>
-          <span>June 24 2021, 13:40 pm</span>
+          </Link>
+          <div className="card__user-name">
+            <p>John Nicholson</p>
+            <span>June 24 2021, 13:40 pm</span>
+          </div>
         </div>
-      </div>
-      <div className="card__body">
-        <p className='card__body__text'>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus
-          excepturi officia quasi quam exercitationem cum dignissimos dolore?
-          Dicta ullam libero omnis. Dolorem fugiat nam sapiente!
-        </p>
-        <img src="./images/feed-image-1.png" className='card__body__post-media' alt="" />
-        <div className="post-row">
+        <div className="card__body">
+          <p className="card__body__text">
+            {post?.text}
+          </p>
+          <img
+            src="./images/feed-image-1.png"
+            className="card__body__post-media"
+            alt=""
+          />
+          <div className="post-row">
             <div className="activity-icons">
-                <div><img src="./images/like-blue.png" width="18" alt="" /> 120</div>
-                <div><img src="./images/comments.png" width="18" alt="" /> 45</div>
-                <div><img src="./images/share.png" width="18" alt="" /> 20</div>
+              <div>
+                <img src="./images/like-blue.png" width="18" alt="" /> 120
+              </div>
+              <div>
+                <img src="./images/comments.png" width="18" alt="" /> 45
+              </div>
+              <div>
+                <img src="./images/share.png" width="18" alt="" /> 20
+              </div>
             </div>
             <div className="post-profile-icon">
-                <img src="./images/profile-pic.png" alt="" />
-                <i className="fas fa-caret-down"></i>
+              <img src="./images/profile-pic.png" alt="" />
+              <i className="fas fa-caret-down"></i>
             </div>
+          </div>
         </div>
-      </div>
-    </div>
+      </div>    
 
-    <div className="card">
-      <div className="card__user-profile">
-        <img src="./images/profile-pic.png" alt="" />
-        <div className="card__user-name">
-          <p>John Nicholson</p>
-          <span>June 24 2021, 13:40 pm</span>
-        </div>
-      </div>
-      <div className="card__body">
-        <p className='card__body__text'>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus
-          excepturi officia quasi quam exercitationem cum dignissimos dolore?
-          Dicta ullam libero omnis. Dolorem fugiat nam sapiente!
-        </p>
-        <img src="./images/feed-image-1.png" className='card__body__post-media' alt="" />
-        <div className="post-row">
-            <div className="activity-icons">
-                <div><img src="./images/like-blue.png" width="18" alt="" /> 120</div>
-                <div><img src="./images/comments.png" width="18" alt="" /> 45</div>
-                <div><img src="./images/share.png" width="18" alt="" /> 20</div>
-            </div>
-            <div className="post-profile-icon">
-                <img src="./images/profile-pic.png" alt="" />
-                <i className="fas fa-caret-down"></i>
-            </div>
-        </div>
-      </div>
-    </div>
-
-    <div className="card">
-      <div className="card__user-profile">
-        <img src="./images/profile-pic.png" alt="" />
-        <div className="card__user-name">
-          <p>John Nicholson</p>
-          <span>June 24 2021, 13:40 pm</span>
-        </div>
-      </div>
-      <div className="card__body">
-        <p className='card__body__text'>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus
-          excepturi officia quasi quam exercitationem cum dignissimos dolore?
-          Dicta ullam libero omnis. Dolorem fugiat nam sapiente!
-        </p>
-        <img src="./images/feed-image-2.png" className='card__body__post-media' alt="" />
-        <div className="post-row">
-            <div className="activity-icons">
-                <div><img src="./images/like-blue.png" width="18" alt="" /> 120</div>
-                <div><img src="./images/comments.png" width="18" alt="" /> 45</div>
-                <div><img src="./images/share.png" width="18" alt="" /> 20</div>
-            </div>
-            <div className="post-profile-icon">
-                <img src="./images/profile-pic.png" alt="" />
-                <i className="fas fa-caret-down"></i>
-            </div>
-        </div>
-      </div>
-    </div>
-
-    <div className="card">
-      <div className="card__user-profile">
-        <img src="./images/profile-pic.png" alt="" />
-        <div className="card__user-name">
-          <p>John Nicholson</p>
-          <span>June 24 2021, 13:40 pm</span>
-        </div>
-      </div>
-      <div className="card__body">
-        <p className='card__body__text'>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus
-          excepturi officia quasi quam exercitationem cum dignissimos dolore?
-          Dicta ullam libero omnis. Dolorem fugiat nam sapiente!
-        </p>
-        <img src="./images/feed-image-3.png" className='card__body__post-media' alt="" />
-        <div className="post-row">
-            <div className="activity-icons">
-                <div><img src="./images/like.png" width="18" alt="" /> 120</div>
-                <div><img src="./images/comments.png" width="18" alt="" /> 45</div>
-                <div><img src="./images/share.png" width="18" alt="" /> 20</div>
-            </div>
-            <div className="post-profile-icon">
-                <img src="./images/profile-pic.png" alt="" />
-                <i className="fas fa-caret-down"></i>
-            </div>
-        </div>
-      </div>
-    </div>
-    <div className="card">
-      <div className="card__user-profile">
-        <img src="./images/profile-pic.png" alt="" />
-        <div className="card__user-name">
-          <p>John Nicholson</p>
-          <span>June 24 2021, 13:40 pm</span>
-        </div>
-      </div>
-      <div className="card__body">
-        <img src="./images/feed-image-4.png" className='card__body__post-media' alt="" />
-        <div className="post-row">
-            <div className="activity-icons">
-                <div><img src="./images/like-blue.png" width="18" alt="" /> 120</div>
-                <div><img src="./images/comments.png" width="18" alt="" /> 45</div>
-                <div><img src="./images/share.png" width="18" alt="" /> 20</div>
-            </div>
-            <div className="post-profile-icon">
-                <img src="./images/profile-pic.png" alt="" />
-                <i className="fas fa-caret-down"></i>
-            </div>
-        </div>
-      </div>
-    </div>
-    <div className="card">
-      <div className="card__user-profile">
-        <img src="./images/profile-pic.png" alt="" />
-        <div className="card__user-name">
-          <p>John Nicholson</p>
-          <span>June 24 2021, 13:40 pm</span>
-        </div>
-      </div>
-      <div className="card__body">
-        <p className='card__body__text'>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus
-        </p>
-        <img src="./images/feed-image-5.png" className='card__body__post-media' alt="" />
-        <div className="post-row">
-            <div className="activity-icons">
-                <div><img src="./images/like-blue.png" width="18" alt="" /> 120</div>
-                <div><img src="./images/comments.png" width="18" alt="" /> 45</div>
-                <div><img src="./images/share.png" width="18" alt="" /> 20</div>
-            </div>
-            <div className="post-profile-icon">
-                <img src="./images/profile-pic.png" alt="" />
-                <i className="fas fa-caret-down"></i>
-            </div>
-        </div>
-      </div>
-    </div>
     </>
   );
-}
+};
 
-export default PostCard
+export default PostCard;
--- src/views/home/Home.jsx
+++ src/views/home/Home.jsx
@@ -1,18 +1,61 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PostCard from '../../components/Post/PostCard';
 import SidebarLeft from '../../components/sidebarLeft/SidebarLeft'
 import { SidebarRight } from '../../components/sidebarRight/SidebarRight'
 
-import {Link} from 'react-router-dom'
+import {Link} from 'react-router-dom';
+
 import './style.scss';
 
 const Home = () => {
+
+   const getPost = JSON.parse(localStorage.getItem('post'))
+  const [post, setPost] = useState(getPost);
+  const [selectFile, setSelectFile] = useState();
+
+  const [input, setInput] = useState('');
+  const [error, setError] = useState('');
+
+  console.log(post);
+  console.log(selectFile);
+
+  const handleChangeImg =(e)=>{
+    let file = e.target.files[0];
+    setSelectFile(file)
+    const reader = new FileReader();
+
+    reader.readAsDataURL(file.result);
+  }
+
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+
+    let id = 0
+    let getId = id ++;
+
+    if(input.length <= 0){
+      setError('Veuillez saisir un message');
+    }else{
+     setPost([...post, {
+        text:input,
+        id: getId,
+        image:''
+       }]);
+      
+      localStorage.setItem('post', JSON.stringify(post)) 
+      ;
+  setInput('');
+    }
+
+  }
+
+
+
   return (
     <>
         <div className="home">
             <SidebarLeft/>
             <div className="container">
-                
                 <div className="story-gallery">
                   <div className="story">
                     <img src="./images/upload.png" alt="upload" className='upload' />
@@ -44,6 +87,8 @@ const Home = () => {
                 </div>
 
                 <div className="write-post-container">
+                  <form  onSubmit={handleSubmit} method="POST">
+
                   <div className="user-profile">
                     <Link to="/profile">
                       <img src="./images/profile-pic.png" alt="" />
@@ -55,17 +100,25 @@ const Home = () => {
                   </div>
 
                   <div className="post-input-container">
-                    <textarea rows="3" placeholder="What's on your mind, John?"></textarea>
+                    <textarea rows="3" placeholder="What's on your mind, John?" value={input} onChange={(e)=> setInput(e.target.value.toLowerCase())}></textarea>
+                      {error && error}
                     <div className="add-post-links">
                       <a href="/"><img src="./images/live-video.png" alt="video" /> Live Video</a>
                       <a href="/"><img src="./images/photo.png" alt="video" /> Photo/Video</a>
+                      <input type="file" onChange={handleChangeImg} id="" />
                       <a href="/"><img src="./images/feeling.png" alt="video" /> Feeling/Activity</a>
                     </div>
+                    <input type='submit' value='submit'/>
                   </div>
+
+                  </form>
                 </div>
 
                 <div className="post__container">
-                  <PostCard/>
+                  {post?.map(item =>(
+                    <PostCard post={item}/>))}
+                  
+                  
                 </div>
 
                 <button className='loadMore'>Load More</button>
